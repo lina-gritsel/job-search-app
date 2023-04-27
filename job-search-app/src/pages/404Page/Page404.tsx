93617c366a3508a9eec3e59d5e07cb7f046135a7
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-04-27 16:21:34 +0300

    feat: create page 404

---
+++ job-search-app/src/pages/404Page/Page404.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+import styles from './Page404.module.scss'
+
+const Page404: FC = () => {
+  return <div className={styles.content}>404Page</div>
+}
+
+export default Page404
--- job-search-app/src/router/routes.tsx
+++ job-search-app/src/router/routes.tsx
@@ -1,8 +1,10 @@
 import { PATHS } from './paths'
 import FavoritesPage from '../pages/FavoritesPage'
 import VacationsPage from '../pages/VacationsPage'
+import Page404 from '../pages/404Page'
 
 export const routes = [
   { path: PATHS.VACATIONS, component: VacationsPage },
   { path: PATHS.FAVORITES, component: FavoritesPage },
+  { path: PATHS.PAGE_404, component: Page404 },
 ]
