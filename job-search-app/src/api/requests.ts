7a3a351d27fe6c909ec89b080441b834afd09ddb
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-05-01 12:43:54 +0300

    feat: create env file

--- job-search-app/src/api/requests.ts
+++ job-search-app/src/api/requests.ts
@@ -3,14 +3,14 @@ import axios from 'axios'
 import { Vacation } from './types'
 
 export const BASE_URL = 'https://startup-summer-2023-proxy.onrender.com/2.0'
+const secretKey = process.env.REACT_APP_X_SECRET_KEY
 
 type FetchAllVacancies = (page: number) => Promise<Vacation[]>
 export const fetchAllVacancies: FetchAllVacancies = async (page) => {
-  console.log(page)
   try {
     const { data } = await axios.get(`${BASE_URL}/vacancies?page=${page}`, {
       headers: {
-        'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
+        'x-secret-key': secretKey,
         'X-Api-App-Id':
           'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
       },
