9524be00b90cd00f994bdc361254200ae3f8b992
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-05-22 17:57:33 +0300

    refactor: add adaptive

--- job-search-app/src/layout/Header/Header.tsx
+++ job-search-app/src/layout/Header/Header.tsx
@@ -7,11 +7,13 @@ import Menu from '../Menu'
 const Header: FC = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.logoWrapper}>
-        <Logo className={styles.logo} />
-        <div className={styles.title}>jobored</div>
+      <div className={styles.content}>
+        <div className={styles.logoWrapper}>
+          <Logo className={styles.logo} />
+          <div className={styles.title}>jobored</div>
+        </div>
+        <Menu />
       </div>
-      <Menu />
     </div>
   )
 }
