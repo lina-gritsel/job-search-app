9c4c7fbdeaf8e25cab02effb3649205d13a26de7
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2023-05-23 15:20:26 +0300

    refactor: change 404 page

--- job-search-app/src/pages/404Page/Page404.tsx
+++ job-search-app/src/pages/404Page/Page404.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react'
 import styles from './Page404.module.scss'
 
 const Page404: FC = () => {
-  return <div className={styles.content}>404Page</div>
+  return <div className={styles.content}>404 error</div>
 }
 
 export default Page404
